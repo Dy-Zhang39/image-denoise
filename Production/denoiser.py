679f01ed8277aec75fcb2738cbc442db3c3ed0e3
Andy0514 <wanghaolinsh@icloud.com>
2022-03-28 13:46:45 -0400

    update denoiser.py to work with CBDnet

--- Production/denoiser.py
+++ Production/denoiser.py
@@ -15,15 +15,24 @@ import utility
 
 PATCH_SIZE = 256
 
-use_cuda = torch.cuda.is_available() and False  # global override
+def tensor_img_to_np(tensor):
+    tensor = tensor.cpu().detach().numpy().squeeze(axis=0)
+    tensor = np.transpose(tensor, [1, 2, 0])
+    tensor = np.clip(tensor, 0, 1)
+    return tensor
+
+use_cuda = torch.cuda.is_available() and True  # global override
 
 Tk().withdraw()
 filename = askopenfilename(filetypes=((("Images"), "*.jpg"), (("All files"), "*.*")))
 
 
 # Todo: save the model in a better place after we obtain a satisfactory set of params
-model = autoencoder.Autoencoder_cnn()
-model.load_state_dict(torch.load("../Baseline/best parameter"))
+#model = autoencoder.Autoencoder_cnn()
+#model.load_state_dict(torch.load("../Baseline/best parameter"))
+model = CNN.CBDnet_1()
+model.load_state_dict(torch.load("../Training/model_parameterlr_1e-05 batch_size_9 weight_decay0.002_best"))
+
 if (use_cuda):
     model.cuda()
 
@@ -48,8 +57,7 @@ num_patch_width = int(desired_width / PATCH_SIZE)
 num_patch_height = int(desired_height / PATCH_SIZE)
 denoised_patches = np.zeros((num_patch_width, num_patch_height, PATCH_SIZE, PATCH_SIZE, 3))
 transform = transforms.Compose(
-        [transforms.ToTensor(),
-          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        [transforms.ToTensor()])
 
 total_patches = num_patch_width * num_patch_height
 current_patch_idx = 0
@@ -59,19 +67,16 @@ for x in range(num_patch_width):
         current_patch_idx += 1
         patch = image_np[y * PATCH_SIZE : (y+1) * PATCH_SIZE,
                                  x * PATCH_SIZE : (x+1) * PATCH_SIZE, :]
+
         patch = transform(patch)
         if (use_cuda):
-            patch.cuda()
-        print(patch.shape)
+            patch = patch.cuda()
         #patch = patch.view(PATCH_SIZE, PATCH_SIZE, 3)
         patch = torch.unsqueeze(patch, 0)
         patch = model(patch)
 
         # Pytorch's output is CHW, but we want to turn it into HWC for numpy
-        patch = patch.squeeze()
-        patch = patch.transpose(0, 2)
-        patch = patch.transpose(0, 1)
-        denoised_patches[x, y] = patch.detach().numpy()
+        denoised_patches[x, y] = tensor_img_to_np(patch)
         print("Progress: {}%".format(100*(current_patch_idx / total_patches)))
 
 
@@ -84,7 +89,13 @@ for x in range(num_patch_width):
 
 
 # crop out the added padding
-denoised_image = denoised_image[pad_top:-pad_bottom, pad_left:-pad_right, :]
-print(denoised_image.shape)
+if (pad_bottom == 0 and pad_right == 0):
+    denoised_image = denoised_image[pad_top:, pad_left:, :]
+elif (pad_bottom == 0):
+    denoised_image = denoised_image[pad_top:, pad_left:-pad_right, :]
+elif (pad_right == 0):
+    denoised_image = denoised_image[pad_top:-pad_bottom, pad_left:, :]
+else:
+    denoised_image = denoised_image[pad_top:-pad_bottom, pad_left:-pad_right, :]
 plt.imshow(denoised_image)
 plt.show()
